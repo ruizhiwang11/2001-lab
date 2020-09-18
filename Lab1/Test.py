c4ce08e14fa00d6d1a0a35364a785aae7ab43756
yiheng123 <yiheng@nutonomy.com>
2020-09-18 19:28:40 +0800

    Add test auto-case generation

--- Lab1/Test.py
+++ Lab1/Test.py
@@ -1,44 +1,75 @@
 from KMP import KmpMatcher
 from BruteForce import BFMatcher
 from BadCharacter import BadCharacter
+import testcase
 import time
-
+import json
+resultlist={}
+bf_time_spent=0
+kmp_time_spent=0
+bc_time_spent=0
 
 def main():
     content = ""
     tic, toc = 0, 0
     with open("./Testing/GCF_000006645.1_ASM664v1_genomic.fna", "r") as fna:
         content = fna.read()
-    testingStr = "TTGGATAC"
+    load_list(content)
 
-    """Brute Force"""
+
+
+"""Brute Force"""
+def Brute_Force(testingStr,content):
     bfmatcher = BFMatcher(testingStr, content)
     tic = time.time()
     bf_result = bfmatcher.bfSearch()
     toc = time.time()
-    print(" - postion list from Brute Force - ")
-    print(bf_result)
+    #print(" - postion list from Brute Force - ")
+    #print(bf_result)
+    global bf_time_spent
     bf_time_spent = toc - tic
-    print("Brute force spent " + str(bf_time_spent))
-    """KMP"""
+    #print("Brute force spent " + str(bf_time_spent))
+
+
+"""KMP"""
+def KMP(testingStr,content):
     kmpMatcher = KmpMatcher(testingStr, content)
     tic = time.time()
     kmp_result = kmpMatcher.kmpSearch()
     toc = time.time()
-    print(" - postion list from KMP - ")
-    print(kmp_result)
+    #print(" - postion list from KMP - ")
+    #print(kmp_result)
+    global kmp_time_spent
     kmp_time_spent = toc - tic
-    print("KMP spent " + str(kmp_time_spent))
-    """Bad Character"""
+    #print("KMP spent " + str(kmp_time_spent))
+
+
+"""Bad Character"""
+def Bad_Character(testingStr,content):
     badCharacter = BadCharacter(testingStr, content)
     tic = time.time()
     bc_result = badCharacter.search()
     toc = time.time()
-    print(" - postion list from Bad Character - ")
-    print(bc_result)
+    #print(" - postion list from Bad Character - ")
+    #print(bc_result)
+    global bc_time_spent
     bc_time_spent = toc - tic
-    print("Bad Character spent " + str(bc_time_spent))
+    #print("Bad Character spent " + str(bc_time_spent))
 
+def load_list(content):
+    testingStrlist = testcase.testinglist
+    for lens in testingStrlist:
+        #print ("\n")
+        print ("There are string of "+ str(lens) +" lens tested: ")
+        #print (testingStrlist[lens])
+        for string in testingStrlist[lens]:
+             Brute_Force(string,content)
+             KMP(string,content)
+             Bad_Character(string,content)
+             resultlist.update({string:[bf_time_spent,kmp_time_spent,bc_time_spent]})
+             print (bf_time_spent,kmp_time_spent,bc_time_spent)
+             print ("\n")
+    print (json.dumps(resultlist, indent=4))
 
 if __name__ == "__main__":
     main()
---
+++ Lab1/testcase.py
@@ -0,0 +1,29 @@
+import random
+import json 
+TestingCharlist=['A','C','G','T','U']
+listGen={}
+tmpgen={}
+testinglist={}
+
+def singStringGen(charlist,lens=int):
+    temp=""
+    for i in range (0,lens):
+        temp += charlist[random.randint(0,4)]
+    #print (temp)
+    #print (len(temp))
+    return temp
+
+def testCaseGen(start,end,numb):
+    for i in range (start,end+1):
+        tmpgen={i:[]}
+        for j in range(numb):
+            x=singStringGen(TestingCharlist,i)
+                    
+            tmpgen[i].append(x)
+                
+        listGen.update(tmpgen)
+        
+    #print (json.dumps(listGen, indent=4))     
+    return listGen 
+
+testinglist=testCaseGen(9,13,5)
