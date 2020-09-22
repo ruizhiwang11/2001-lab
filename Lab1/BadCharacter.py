6364500fc537623c27a7729631c08d347a5e741b
leonard <leonardllzznn@gmail.com>
2020-09-22 22:02:59 +0800

    final bc

--- Lab1/BadCharacter.py
+++ Lab1/BadCharacter.py
@@ -12,9 +12,7 @@ class BadCharacter(object):
         # need a dictionary with pattern as key and last occurrence as value
         pattern_chars_dict = {}
         for i in range(len(self._pattern)):
-            actual_char = self._pattern[i]
-            maxshift = max(1,len(self._pattern) - i -1)
-            pattern_chars_dict[actual_char] = maxshift
+            pattern_chars_dict[self._pattern[i]] = i
         return pattern_chars_dict
 
     def search(self) -> List[int]:
@@ -29,9 +27,12 @@ class BadCharacter(object):
             bad_character_pos = 0
             # checking mismatch position
             for i in range(p_len - 1, -1, -1):
-                if(p[i] != s[i+s_pos]):
-                    if s[i+s_pos] in pattern_chars_dict:
-                        bad_character_pos = pattern_chars_dict[s[i+s_pos]]
+                if p[i] != s[i + s_pos]:
+                    if (
+                        s[i + s_pos] in pattern_chars_dict
+                        and i - pattern_chars_dict[s[i + s_pos]] > 0
+                    ):
+                        bad_character_pos = i - pattern_chars_dict[s[i + s_pos]]
                         break
                     else:
                         bad_character_pos = i + 1
@@ -40,7 +41,7 @@ class BadCharacter(object):
                 # append position index to result list since no mismtach found
                 found_index_list.append(s_pos + 1)
             if bad_character_pos == 0:
-                s_pos +=1
+                s_pos += 1
             else:
                 s_pos += bad_character_pos
                 # the shift space is determined by the bad character table
